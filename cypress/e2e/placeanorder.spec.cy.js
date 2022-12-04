46c34f280d8d5010945f1fb941f8d02cfc2929d2
Anmol Zahra <zahra.anmol@invitae.com>
2022-12-05 02:20:48 +0500

    Update placeanorder.spec.cy.js

--- cypress/e2e/placeanorder.spec.cy.js
+++ cypress/e2e/placeanorder.spec.cy.js
@@ -1,32 +1,32 @@
-import cypressConfig from '../../cypress.config';
+//import cypressConfig from '../../cypress.config';
 import * as p from '../fixtures/data.json';
 import * as locator from '../support/e2e.js';
 import {VisitnopCommerceStore,addtocart,gotocart,checkout} from '../support/commands'
 
 
 describe('Go to Website', () => {
-  var url= cypressConfig.env.baseUrl
+  // /var url= cypressConfig.env.baseUrl
   it('navigates to URL!', () => {
-    //cy.VisitnopCommerceStore(url)
+    cy.VisitnopCommerceStore("https://demo.nopcommerce.com/")
   })
-  // it('searches Product and adds to cart!', () => {
-  //   cy.addtocart(p.productName)
-      
-  // })
-  // it('Go to Cart!', () => {   
-  //   cy.gotocart()
-  // })
-  
-  // it('Verify that the right product has been added!', () => {
-  //   cy.get(locator.productName).then(function($elem) {
-  //     var Name=$elem.text()
-  //     expect(Name).to.equal(p.productName)  
-  //   })
-  // })
-  // it('checksout', () => {
-  //  cy.checkout()
+  it('searches Product and adds to cart!', () => {
+    cy.addtocart(p.productName)
     
-  // })
+  })
+  it('Go to Cart!', () => {   
+    cy.gotocart()
+  })
+  
+  it('Verify that the right product has been added!', () => {
+    cy.get(locator.productName).then(function($elem) {
+      var Name=$elem.text()
+      expect(Name).to.equal(p.productName)  
+    })
+  })
+  it('checksout', () => {
+    cy.checkout()
+    
+  })
 })
 
 
